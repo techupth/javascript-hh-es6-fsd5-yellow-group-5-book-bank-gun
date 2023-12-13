c364a3c6c7bf30b9d18852fef5fc17e33ac48a2a
Gun <cobaltgun2324@gmail.com>
2023-12-13 12:39:01 +0700

    feat: add complete exercise

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,17 @@
+// let height = undefined;
+// let result = nullish(height);
+
+// function nullish(height) {
+//   if (height === null || height === undefined) {
+//     return "Height is not defined";
+//   }
+//   return height;
+// }
+
+// console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
+
+// Exercise #6: Nullish Coalescing
 let height = undefined;
-let result;
+let result = height ?? "Height is not defined";
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
